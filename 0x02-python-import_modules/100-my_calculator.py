1d7c1700743fdf9a52f0fc6ca4af6a1fc75d0d5c
amal2002lazrak <amallazrak221@gmail.com>
2023-11-30 22:53:15 +0100

    updated

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    import sys
+
+a = 10
+b = 5
+n = len(sys.argv) - 1
+if n < 3:
+    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+    sys.exit(1)
+if op == '+' or op == '-' or op == '*' or op == '/':
+    print("{} + {} = {}" .format(a, b, add(a, b)))
+    print("{} - {} = {}" .format(a, b, sub(a, b)))
+    print("{} * {} = {}" .format(a, b, mul(a, b)))
+    print("{} / {} = {}" .format(a, b, div(a, b)))
