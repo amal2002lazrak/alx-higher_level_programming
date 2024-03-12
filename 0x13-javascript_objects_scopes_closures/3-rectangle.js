5f09e8db28ff7ad791e39787c20c43d02c9b98fc
amal2002lazrak <amallazrak221@gmail.com>
2024-03-13 00:04:25 +0100

    updated

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    } else {
+      Object.create(null);
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let str = ''; /* empty str */
+      for (let j = 0; j < this.width; j++) {
+        str += 'X';
+      }
+      console.log(str);
+    }
+  }
+}
+
+module.exports = Rectangle;
